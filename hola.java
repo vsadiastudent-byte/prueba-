b1532caff66ca59a797d7e8fee6c5e963b524984
victor230304 <vsadia.student@air-institute.com>
2026-01-29 19:10:07 +0100

    Añadir métodos de saludo y suma a la clase hola

--- hola.java
+++ hola.java
@@ -2,5 +2,12 @@ public class hola {
     public static void main(String[] args) {
         System.out.println("Hola, mundo!");
     }
+    public void saludo() {
+        System.out.println("¡Saludos desde la clase hola!");
+    }
+
+    public int sumar(int a, int b) {
+        return a + b;
+    }
 
 }
